3076ecb18bf8f7d7863aabf2dc24bc6c25556027
Gultekin Gonen <tekingonen14@gmail.com>
2025-01-21 20:08:22 +0100

    after database configuration

---
+++ src/main/java/be/iccbxl/pid/ProjetReservationApplication.java
@@ -0,0 +1,13 @@
+package be.iccbxl.pid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/iccbxl/pid/ProjetReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package be.iccbxl.pid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
