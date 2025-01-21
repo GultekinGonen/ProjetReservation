c60c903e9ae94b09491d28a0dc3c26d0afb6965d
Gultekin Gonen <tekingonen14@gmail.com>
2025-01-21 20:49:02 +0100

    Creation de la class ArtistController, fichier sql V1_2__insert_artists_table ,

--- src/main/java/be/iccbxl/pid/controller/ArtistController.java
+++ src/main/java/be/iccbxl/pid/controller/ArtistController.java
@@ -1,4 +1,30 @@
 package be.iccbxl.pid.controller;
 
+import be.iccbxl.pid.model.Artist;
+import be.iccbxl.pid.service.ArtistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ArtistController {
+    @Autowired
+    ArtistService service;
+
+    @GetMapping("/artists")
+    public String index() {
+        StringBuilder content = new StringBuilder("<ul>");
+
+        List<Artist> artists = service.getAllArtists();
+        artists.forEach(artist -> {
+            content.append("<li>"+artist+"</li>");
+        });
+        content.append("</ul>");
+
+        return content.toString();
+    }
+
 }
+
